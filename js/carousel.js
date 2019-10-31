18fe2b2c3b8b455cceef39e71d725d2367ff61d2
nischal <niscalbhandari12@gmail.com>
2019-10-31 22:55:10 +0545

    window onload

--- js/carousel.js
+++ js/carousel.js
@@ -1,42 +1,31 @@
-/*var items=document.getElementsByClassName('team-wrapper');
-var value=0;
-var direction=true;
-var position=0;
-setInterval(function(){
-	var xyz=value;
-	console.log(items[0].clientWidth);
-	for(var i=0;i<items.length;i++){
-
-		console.log(value);
-		items[i].style.marginLeft=xyz+'px';
-	}
-*/
-
-	/*items[1].style.marginLeft=value+'px';
-	if(value<-(items[0].clientWidth*(items.length-1))){
-		direction=false;
-	}
-	if(value>0){
-		direction=true;
-	}
-	if(direction){
-		value-=10;
-	}
-	else{
-		value+=10;
-	}
-
-},100);*/
+var menuBurger=document.getElementsByClassName('header-burger');
 var item=document.getElementsByClassName('our-team-body');
 var objects=document.getElementsByClassName('team-wrapper');
 var indicators=document.getElementsByClassName('slider-button');
 var bodyWidths=document.getElementsByClassName('our-team-body');
 var bodyWidth=bodyWidths[0];
+var menuShow=menuBurger[0];
+var menuArray=document.getElementsByClassName('header-bottom');
+var menu=menuArray[0];
 var IndexValue=0;
 
 var direction=false;
 var value=0;
 var count=0;
+var show=true;
+menuShow.addEventListener('click',function(){
+	console.log("clicked");
+	if(show){
+		menu.style.display="block";
+		show=!show;
+	}
+	else{
+		menu.style.display='none';
+		show=!show;
+	}
+})
+window.addEventListener("load",myCarousel);
+function myCarousel(){
 bodyWidth.style.width=objects.length*objects[0].clientWidth+'px';
 setInterval(function(){
 console.log(objects[0].clientWidth);
@@ -60,5 +49,5 @@ if(count===500){
 }
 },10);	
 
-
+}
 
